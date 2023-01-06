cd4596fad9f2d6ea6a61535e5d5d846fa348338d
Singstad90 <bjorn_sing@hotmail.com>
2023-01-06 12:45:00 +0100

    Update your_model.py

--- code/models/your_model.py
+++ code/models/your_model.py
@@ -4,16 +4,19 @@ import numpy as np
 
 
 class inception_time_model(ClassificationModel):
-    def __init__(self, name, n_classes,  sampling_frequency, outputfolder, input_shape):
+    def __init__(self, name, n_classes,  sampling_frequency, outputfolder, input_shape, epoch=30, batch_size=32, lr_init = 0.001, lr_red="yes", model_depth=6, loss="bce", kernel_size=40, bottleneck_size=32, nb_filters=32, clf="binary"):
         super(inception_time_model, self).__init__()
         self.name = name
         self.n_classes = n_classes
         self.sampling_frequency = sampling_frequency
         self.outputfolder = outputfolder
         self.input_shape = input_shape
-        self.model = build_model((self.sampling_frequency*10,12),self.n_classes)
-        self.epoch = 30 
-        self.batch_size =32
+        self.model = build_model((self.sampling_frequency*10,12),self.n_classes,lr_init = 0.001, depth=model_depth, kernel_size=40, bottleneck_size=32, nb_filters=32,clf="binary")
+        self.epoch = epoch 
+        self.batch_size = batch_size
+        self.lr_red = lr_red
+        self.loss = loss
+        
 
     def fit(self, X_train, y_train, X_val, y_val):
         self.model.fit(X_train, y_train, epochs=self.epoch, batch_size=self.batch_size, 
@@ -23,7 +26,7 @@ class inception_time_model(ClassificationModel):
         return self.model.predict(X)
 
 
-def _inception_module(input_tensor, stride=1, activation='linear', use_bottleneck=True, kernel_size=40, bottleneck_size=64, nb_filters=64):
+def _inception_module(input_tensor, stride=1, activation='linear', use_bottleneck=True, kernel_size=40, bottleneck_size=32, nb_filters=32):
 
     if use_bottleneck and int(input_tensor.shape[-1]) > 1:
         input_inception = tf.keras.layers.Conv1D(filters=bottleneck_size, kernel_size=1,
@@ -62,7 +65,7 @@ def _shortcut_layer(input_tensor, out_tensor):
     x = tf.keras.layers.Activation('relu')(x)
     return x
 
-def build_model(input_shape, nb_classes, depth=10, use_residual=True):
+def build_model(input_shape, nb_classes, depth=6, use_residual=True, lr_init = 0.001, kernel_size=40, bottleneck_size=32, nb_filters=32, clf="binary"):
     input_layer = tf.keras.layers.Input(input_shape)
 
     x = input_layer
@@ -70,18 +73,20 @@ def build_model(input_shape, nb_classes, depth=10, use_residual=True):
 
     for d in range(depth):
 
-        x = _inception_module(x)
+        x = _inception_module(x,kernel_size = kernel_size, bottleneck_size=bottleneck_size, nb_filters=nb_filters)
 
         if use_residual and d % 3 == 2:
             x = _shortcut_layer(input_res, x)
             input_res = x
 
     gap_layer = tf.keras.layers.GlobalAveragePooling1D()(x)
-
-    output_layer = tf.keras.layers.Dense(units=nb_classes,activation='sigmoid')(gap_layer)
-
+    if clf == "binary":
+        output_layer = tf.keras.layers.Dense(units=nb_classes,activation='sigmoid')(gap_layer)
+    elif clf == "cat"
+        output_layer = tf.keras.layers.Dense(units=nb_classes,activation='softmax')(gap_layer)
+        
     model = tf.keras.models.Model(inputs=input_layer, outputs=output_layer)
-    model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), 
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate=lr_init), 
                   metrics=[tf.keras.metrics.BinaryAccuracy(),
                            tf.keras.metrics.AUC(
                         num_thresholds=200,
@@ -102,11 +107,7 @@ def build_model(input_shape, nb_classes, depth=10, use_residual=True):
     return model
 
 def scheduler(epoch, lr):
-    if epoch == 15:
-        return lr*0.1
-    elif epoch == 30:
-        return lr*0.1
-    elif epoch == 30:
+    if i%5 == 0:
         return lr*0.1
     else:
         return lr
